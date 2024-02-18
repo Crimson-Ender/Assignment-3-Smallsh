63ed735d8c8731472f6e3639562417d36e737c6d
Crimson-Ender <crimsonender99@gmail.com>
2024-02-18 14:43:45 -0800

    variable expansion v1

--- shell.c
+++ shell.c
@@ -10,32 +10,33 @@
 
 #include "shell.h"
 
+int running = 1;
+
 int main(){
     startup_text();
 
     int num_chars_entered = -5;
     int current_char = -5;
     size_t buffer_size = 0;
-    char* line_entered = NULL;
+    char* line = NULL;
     
-    while(1){
-        printf("%d:", getpid());
+    do{
+        printf("%d: ", getpid());
         fflush(stdin);
-        num_chars_entered = getline(&line_entered, &buffer_size, stdin);
+        num_chars_entered = getline(&line, &buffer_size, stdin);
 
-        // if(line_entered[0] = '\n'){
-
-        // }
-        if(check_blank(line_entered) == 1){
+        if(strlen(line)==0|| strcmp(line, "\n")==0){
             printf("empty line");
-        }
-        else{
-            printf("line entered: %s\n",line_entered);
+        }else{
+            if(strstr(line, "$$")){
+                line = variable_expansion(line);
+            }
+            parse_command(line);
         }
 
-        free(line_entered);
-        line_entered = NULL;
-    }
+        free(line);
+        line = NULL;
+    }while(running == 1);
 
 
     printf("\n");
@@ -75,4 +76,74 @@ int check_blank(char* line){
         }
     }
     return is_blank;
+}
+
+void parse_command(char* line){
+    char* save_string; //to prvent the command the user passed from getting ruined in the token process
+    char* command[256];
+
+    char* token = strtok_r(line, " ", &save_string);
+
+    strcpy(command, token);
+
+    if(strcmp(command, "#")==0){
+        //this is a comment, we do not process anything from here.
+        printf("comment");
+        return;
+    }else if(strlen(command)==0){
+        //printf("empty line");
+        return;
+    }else{
+        
+        char* arg1 = strtok_r(NULL, " ", &save_string);
+        char* arg2 = strtok_r(NULL, " ", &save_string);
+
+        //printf("DEBUG: command: %s, argument 1: %s, argument 2: %s\n", command, arg1, arg2);
+        process_command(command, arg1, arg2);
+    }
+
+}
+
+void process_command(char* command, char* arg1, char* arg2){
+
+    if(strcmp(command, "cd")==0 || strcmp(command, "cd\n")==0){
+        printf("this is what would change directory");
+    }else if(strcmp(command, "status")==0){
+        printf("this would print status");
+    }else if(strcmp(command, "exit")==0){
+        //this will end the program
+        running = 0;
+        return;
+    }else if(strcmp(command, "#")==0){
+        return;
+    }else{
+        //this will run a command that is not build in
+    }
+
+}
+
+void change_directory(char* command, char* arg1){
+
+}
+
+char* variable_expansion(char* line){
+    char* expanded_string = malloc(strlen(line)+(sizeof(int)*10));
+    char* variable[10];
+    sprintf(variable, "%d", getpid());
+    int i, j = 0;
+
+    for(i = 0; i<strlen(line); i++){
+        if(line[i] == '$'){
+            strcat(expanded_string,variable);
+            i++;
+        }else{
+            expanded_string[j] = line[i];
+        }
+        j++;
+    }
+
+    printf("Expanded string == %s", expanded_string);
+
+    free(line);
+    return expanded_string;
 }
--- shell.h
+++ shell.h
@@ -1,1 +1,10 @@
+#ifndef __SHELL_H
+#define __SHELL_H
+
 void startup_text();
+int check_blank(char* line);
+void parse_command(char* line);
+void process_command(char* command, char* arg1, char* arg2);
+char* variable_expansion(char* line);
+
+#endif
